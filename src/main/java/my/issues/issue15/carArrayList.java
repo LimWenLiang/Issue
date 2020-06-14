a75fc48d640e200417be4d53d85cce8c13829f6a
LimWenLiang <lwiang98@gmail.com>
2020-06-15 00:42:40 +0800

    update issue 15

--- src/main/java/my/issues/issue15/carArrayList.java
+++ src/main/java/my/issues/issue15/carArrayList.java
@@ -4,35 +4,102 @@ import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+
 public class carArrayList {
-    static ArrayList carModel = new ArrayList();
+    static ArrayList car = new ArrayList();
+
+    static class Add implements Runnable {
+        @Override
+        public void run() {
+
+            // 1. Add 3 objects to an ArrayList
+            car.add("Proton");
+            car.add("Perdua");
+            car.add("Yamaha");
+            System.out.println("1. " + car);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // 3. Add new object
+            car.add("Honda");
+            System.out.println("3. " + car);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // 5. Add a new object
+            car.add("Nissan");
+            System.out.println("5. " + car);
+
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // 6. Add again a new object
+            car.add("Toyota");
+            System.out.println("6. " + car);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    static class Delete implements Runnable {
+        @Override
+        public void run() {
+
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // 2. Remove the last object
+            car.remove(2);
+            System.out.println("2. " + car);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // 4. Remove the 1st object
+            car.remove(0);
+            System.out.println("4. " + car);
+
+            try {
+                Thread.sleep(1500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // 7. Remove the 2nd object
+            car.remove(1);
+            System.out.println("7. " + car);
+        }
+    }
 
     public static void main(String[] args) {
-
         ExecutorService service = Executors.newFixedThreadPool(2);
+        service.execute(new Add());
+        service.execute(new Delete());
 
-        service.execute(new Task1());
-        service.execute(new Task2());
         service.shutdown();
-
     }
 
-    static class Task1 implements Runnable {
-        @Override
-        public void run() {
-            System.out.println("Thread Name: " + Thread.currentThread().getName());
-            carModel.add("Proton");
-            carModel.add("Yamaha");
-            carModel.add("Nissan");
 
-            System.out.println(carModel);
-        }
-    }
-
-    static class Task2 implements Runnable {
-        @Override
-        public void run() {
-
-        }
-    }
 }
