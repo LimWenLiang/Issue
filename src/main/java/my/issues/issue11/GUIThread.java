1353fabdbbf6ed2a1b6746b2c5a0008d4c1435a6
LimWenLiang <lwiang98@gmail.com>
2020-07-20 01:31:48 +0800

    update issue11

--- src/main/java/my/issues/issue11/GUIThread.java
+++ src/main/java/my/issues/issue11/GUIThread.java
@@ -14,7 +14,7 @@ public class GUIThread {
     private JTextArea output;
 
     public GUIThread() {
-        mainFrame = new JFrame("R_Multiplication");
+        mainFrame = new JFrame("MyThread");
         mainFrame.setSize(500, 250);
         mainFrame.setLayout(null);
         mainFrame.setDefaultCloseOperation(3);
